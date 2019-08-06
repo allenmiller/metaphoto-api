735aa8582c2064050ebecb0bf6c39011020ecba0
Al Miller <al.miller@ajmiller.net>
2019-08-06 10:22:02 -0700

    remove the time endpoint

--- api/time.js
+++
@@ -1,7 +0,0 @@
-const utils = require('./utils');
-exports.handler = (event, context, callback) => {
-    let currentTime = new Date();
-    let response = utils.buildResponse("200", '{"data":"' + currentTime.toString() + '"}');
-    console.log("AJM: ", response);
-    callback(null, response);
-};
