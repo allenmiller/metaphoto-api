0b5ca26ad0f0578c10acc2f65c587a7b520e66c0
Al Miller <al.miller@ajmiller.net>
2019-08-06 09:47:43 -0700

    remove commented code

--- api/utils.js
+++ api/utils.js
@@ -2,7 +2,6 @@ module.exports = {
 
     buildResponse: buildResponse = (statusCode, body) => {
         let headers = {
-//            'Access-Control-Allow-Origin': process.env.WEB_APP_URL,
             'Access-Control-Allow-Origin': "*",
             'Access-Control-Allow-Credentials': true,
             'Content-Type': 'application/json'
