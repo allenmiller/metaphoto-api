b309c306819b99a4aabb039397c689c6c77b1ee1
Al Miller <al.miller@ajmiller.net>
2019-08-07 19:59:44 -0700

    add path param

--- api/filmstock/delete.js
+++ api/filmstock/delete.js
@@ -7,23 +7,17 @@ const buildResponse = utils.buildResponse;
 exports.delete = (event, context, callback) => {
     console.log('AJM delete(): Received event:', JSON.stringify(event));
     console.log('context:', JSON.stringify(context));
-    const done = (err, res) => {
-        console.log("AJM: in done()", err, res);
-        callback(null, buildResponse(
-            err ? '400' : '200',
-            err ? err.message : JSON.stringify(res)
-        ));
-    };
 
     const dynamodb = new AWS.DynamoDB.DocumentClient();
 
     let deleteParams = {
         TableName: MEDIA_TABLE_NAME,
-        Key: {"primaryHashKey": "ABC"}
+        Key: {
+            "primaryHashKey": "ABC"
+        }
     };
 
-    // TODO: add check to prevent deletion of filmstock items
-    // pointed to by film items.
+    // TODO: add check to prevent deletion of filmstock item pointed to by film items.
     dynamodb.delete(deleteParams, (err, data) => {
         if (err) {
             console.log("ERROR deleting item: ", err);
@@ -31,7 +25,6 @@ exports.delete = (event, context, callback) => {
         } else {
             console.log("AJM: back from delete(): ", data);
             callback(null, buildResponse('200', `item deleted`));
-
         }
     });
 };
